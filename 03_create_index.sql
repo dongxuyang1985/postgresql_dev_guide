f1b87d0cad27769ed356517fe011b5a6696ee2ed
Tony Dong <bell-1985@163.com>
2022-04-11 21:48:14 +0800

    Create 03_create_index.sql

---
+++ 03_create_index.sql
@@ -0,0 +1,14 @@
+CREATE INDEX emp_department_ix
+       ON employees (department_id);
+
+CREATE INDEX emp_job_ix
+       ON employees (job_id);
+
+CREATE INDEX emp_manager_ix
+       ON employees (manager_id);
+
+CREATE INDEX emp_name_ix
+       ON employees (last_name, first_name);
+
+CREATE INDEX dept_location_ix
+       ON departments (location_id);
