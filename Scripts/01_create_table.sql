84da33ec68d6fb600c71de9c5af627914d86c277
Tony Dong <bell-1985@163.com>
2022-07-09 22:21:55 +0800

    Update 01_create_table.sql

--- Scripts/01_create_table.sql
+++ Scripts/01_create_table.sql
@@ -1,37 +1,38 @@
+-- 部门信息表
 CREATE TABLE departments
-    ( department_id    INTEGER NOT NULL
-    , department_name  CHARACTER VARYING(30) NOT NULL
-    , manager_id       INTEGER
-    , location_id      INTEGER
-	, CONSTRAINT dept_id_pk PRIMARY KEY (department_id)
+    ( department_id    INTEGER NOT NULL -- 部门编号，主键
+    , department_name  CHARACTER VARYING(30) NOT NULL -- 部门名称
+    , manager_id       INTEGER -- 部门经理的员工编号
+    , location_id      INTEGER -- 部门所在位置编号
+    , CONSTRAINT dept_id_pk PRIMARY KEY (department_id)
     ) ;
 
-
+-- 职位信息表
 CREATE TABLE jobs
-    ( job_id         CHARACTER VARYING(10) NOT NULL
-    , job_title      CHARACTER VARYING(35) NOT NULL
-    , min_salary     INTEGER
-    , max_salary     INTEGER
-	, CONSTRAINT job_id_pk PRIMARY KEY(job_id)
+    ( job_id         CHARACTER VARYING(10) NOT NULL -- 职位编号，主键
+    , job_title      CHARACTER VARYING(35) NOT NULL -- 职位名称
+    , min_salary     INTEGER -- 最低月薪
+    , max_salary     INTEGER -- 最高月薪
+    , CONSTRAINT job_id_pk PRIMARY KEY(job_id)
     ) ;
 
-
+-- 员工信息表
 CREATE TABLE employees
-    ( employee_id    INTEGER NOT NULL
-    , first_name     CHARACTER VARYING(20)
-    , last_name      CHARACTER VARYING(25) NOT NULL
-    , email          CHARACTER VARYING(25) NOT NULL
-    , phone_number   CHARACTER VARYING(20)
-    , hire_date      DATE NOT NULL
-    , job_id         CHARACTER VARYING(10) NOT NULL
-    , salary         NUMERIC(8,2)
-    , commission_pct NUMERIC(2,2)
-    , manager_id     INTEGER
-    , department_id  INTEGER
-	, CONSTRAINT emp_emp_id_pk PRIMARY KEY (employee_id)
+    ( employee_id    INTEGER NOT NULL -- 员工编号，主键
+    , first_name     CHARACTER VARYING(20) -- 名字
+    , last_name      CHARACTER VARYING(25) NOT NULL -- 姓氏
+    , email          CHARACTER VARYING(25) NOT NULL -- 邮箱
+    , phone_number   CHARACTER VARYING(20) -- 电话号码
+    , hire_date      DATE NOT NULL -- 入职日期
+    , job_id         CHARACTER VARYING(10) NOT NULL -- 职位编号
+    , salary         NUMERIC(8,2) -- 月薪
+    , commission_pct NUMERIC(2,2) -- 销售提成百分比
+    , manager_id     INTEGER -- 经理
+    , department_id  INTEGER -- 部门编号
+    , CONSTRAINT emp_emp_id_pk PRIMARY KEY (employee_id)
     , CONSTRAINT emp_salary_min CHECK (salary > 0) 
     , CONSTRAINT emp_email_uk UNIQUE (email)
-	, CONSTRAINT emp_dept_fk FOREIGN KEY (department_id) REFERENCES departments(department_id)
+    , CONSTRAINT emp_dept_fk FOREIGN KEY (department_id) REFERENCES departments(department_id)
     , CONSTRAINT emp_job_fk FOREIGN KEY (job_id) REFERENCES jobs(job_id)
     , CONSTRAINT emp_manager_fk FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
     ) ;
@@ -39,7 +40,7 @@ CREATE TABLE employees
 ALTER TABLE departments
 ADD CONSTRAINT dept_mgr_fk FOREIGN KEY (manager_id) REFERENCES employees(employee_id);
 
-
+-- 员工详细信息视图
 CREATE VIEW emp_details_view
   (employee_id,
    job_id,
